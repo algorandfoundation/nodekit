7e5ac6bafb5f7c8f8a8ff87ae13435a3a9949ffa
Michael Feher <github@phearzero.com>
2025-03-21 08:27:07 -0400

    docs(tape): dev mode for previews

--- docs/src/content.config.ts
+++ docs/src/content.config.ts
@@ -1,8 +1,9 @@
 import { defineCollection } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
+import {isDev} from "./dev.ts";
 
 export const collections: any = {}
-if(import.meta.env.DEV) {
+if(isDev()) {
 	collections.docs = defineCollection({ loader: docsLoader(), schema: docsSchema() })
 }
